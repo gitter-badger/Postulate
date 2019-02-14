bf6a035c1dc06f338217ee8a0af905d06f7a3c02
adamo <adamosoftware@gmail.com>
2019-02-14 08:19:23 -0500

    added SaveAsync that can target select properties

--- Postulate.Base/CommandProvider_SaveOverloads.cs
+++ Postulate.Base/CommandProvider_SaveOverloads.cs
@@ -1,12 +1,27 @@
 ﻿using Postulate.Base.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Postulate.Base
 {
 	public abstract partial class CommandProvider<TKey>
 	{
+		public async Task<TKey> SaveAsync<TModel>(IDbConnection connection, TModel @object, IUser user, params Expression<Func<TModel, object>>[] setColumns) where TModel : new()
+		{
+			if (IsNew(@object))
+			{
+				return await SaveAsync(connection, @object, user);
+			}
+			else
+			{
+				await UpdateAsync(connection, @object, user, setColumns);
+				return GetIdentity(@object);
+			}
+		}
+
 		/// <summary>
 		/// Inserts or creates TTarget from explicitly set properties in TSource and returns the inserted or updated id value
 		/// </summary>
