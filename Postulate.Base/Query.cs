c9e2d090bee541bd0a00dd49a8561c2e9800e41a
adamo <adamosoftware@gmail.com>
2019-02-23 11:55:40 -0500

    base version 1.3.7 new QueryUtil class

--- Postulate.Base/Query.cs
+++ Postulate.Base/Query.cs
@@ -26,8 +26,8 @@ namespace Postulate.Base
 
 		public IEnumerable<TResult> Execute(IDbConnection connection)
 		{
-			ResolvedSql = ResolveQuery(this, out DynamicParameters queryParams);
-			
+			ResolvedSql = QueryUtil.ResolveSql(Sql, this, out DynamicParameters queryParams);
+
 			try
 			{
 				return connection.Query<TResult>(ResolvedSql, queryParams);
@@ -40,7 +40,7 @@ namespace Postulate.Base
 
 		public TResult ExecuteSingle(IDbConnection connection)
 		{
-			ResolvedSql = ResolveQuery(this, out DynamicParameters queryParams);
+			ResolvedSql = QueryUtil.ResolveSql(Sql, this, out DynamicParameters queryParams);
 
 			try
 			{
@@ -49,13 +49,12 @@ namespace Postulate.Base
 			catch (Exception exc)
 			{
 				throw new QueryException(exc, ResolvedSql, queryParams);
-			}
-			
+			}			
 		}
 
 		public async Task<TResult> ExecuteSingleAsync(IDbConnection connection)
 		{
-			ResolvedSql = ResolveQuery(this, out DynamicParameters queryParams);
+			ResolvedSql = QueryUtil.ResolveSql(Sql, this, out DynamicParameters queryParams);
 
 			try
 			{
@@ -69,7 +68,7 @@ namespace Postulate.Base
 
 		public async Task<IEnumerable<TResult>> ExecuteAsync(IDbConnection connection)
 		{
-			ResolvedSql = ResolveQuery(this, out DynamicParameters queryParams);
+			ResolvedSql = QueryUtil.ResolveSql(Sql, this, out DynamicParameters queryParams);
 
 			try
 			{
@@ -81,88 +80,6 @@ namespace Postulate.Base
 			}			
 		}
 
-		private static string ResolveQuery(Query<TResult> query, out DynamicParameters queryParams)
-		{
-			string result = query.Sql;
-
-			List<string> terms = new List<string>();
-			var queryProps = GetProperties(query, result, out IEnumerable<string> builtInParams);
-
-			queryParams = new DynamicParameters();
-			foreach (var prop in queryProps)
-			{
-				var value = prop.GetValue(query);
-				if (value != null) queryParams.Add(prop.Name, value);
-			}
-
-			Dictionary<string, string> whereBuilder = new Dictionary<string, string>()
-			{
-				{ InternalStringExtensions.WhereToken, "WHERE" }, // query has no WHERE clause, so it will be added
-				{ InternalStringExtensions.AndWhereToken, "AND" } // query already contains a WHERE clause, we're just adding to it
-			};
-
-			string token;
-			if (result.ContainsAny(whereBuilder.Select(kp => kp.Key), out token))
-			{
-				// loop through this query's properties, but ignore base properties (like ResolvedSql and TraceCallback) since they are never part of WHERE clause
-				foreach (var pi in queryProps)
-				{
-					object value = pi.GetValue(query);
-					if (HasValue(value))
-					{
-						// built-in params are not part of the WHERE clause, so they are excluded from added terms
-						if (!builtInParams.Contains(pi.Name.ToLower()))
-						{
-							var cases = pi.GetCustomAttributes(typeof(CaseAttribute), false).OfType<CaseAttribute>();
-							var selectedCase = cases?.FirstOrDefault(c => c.Value.Equals(value));
-							if (selectedCase != null)
-							{
-								terms.Add(selectedCase.Expression);
-							}
-							else
-							{
-								WhereAttribute whereAttr = pi.GetAttribute<WhereAttribute>();
-								if (whereAttr != null) terms.Add(whereAttr.Expression);
-							}
-						}
-					}
-				}
-
-				result = result.Replace(token, (terms.Any()) ? $"{whereBuilder[token]} {string.Join(" AND ", terms)}" : string.Empty);
-			}
-
-			return result;
-		}
-
-		private static bool HasValue(object value)
-		{
-			if (value != null)
-			{
-				if (value.Equals(string.Empty)) return false;
-				return true;
-			}
-
-			return false;
-		}
-
-		/// <summary>
-		/// Returns the properties of a query object based on parameters defined in a
-		/// SQL statement as well as properties with Where and Case attributes
-		/// </summary>
-		private static IEnumerable<PropertyInfo> GetProperties(object query, string sql, out IEnumerable<string> builtInParams)
-		{
-			// this gets the param names within the query based on words with leading '@'
-			builtInParams = sql.GetParameterNames(true).Select(p => p.ToLower());
-			var builtInParamsArray = builtInParams.ToArray();
-
-			// these are the properties of the Query that are explicitly defined and may impact the WHERE clause
-			var queryProps = query.GetType().GetProperties().Where(pi =>
-				pi.HasAttribute<WhereAttribute>() ||
-				pi.HasAttribute<CaseAttribute>() ||
-				builtInParamsArray.Contains(pi.Name.ToLower()));
-
-			return queryProps;
-		}
 
 		/// <summary>
 		/// Intended for implementing <see cref="Interfaces.ITestableQuery"/> for unit testing, not intended for use on its own
@@ -171,7 +88,7 @@ namespace Postulate.Base
 		{
 			try
 			{
-				ResolvedSql = ResolveQuery(this, out DynamicParameters queryParams);
+				ResolvedSql = QueryUtil.ResolveSql(Sql, this, out DynamicParameters queryParams);
 				return connection.Query(ResolvedSql, queryParams);
 			}
 			catch (Exception exc)
---
+++ Postulate.Base/QueryUtil.cs
@@ -0,0 +1,133 @@
+﻿using Dapper;
+using Postulate.Base.Attributes;
+using Postulate.Base.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Postulate.Base
+{
+	public static class QueryUtil
+	{
+		public static string ResolveSql(string sql, object parameters = null)
+		{
+			return ResolveSql(sql, parameters, out DynamicParameters queryParams);
+		}
+
+		public static string ResolveSql(string sql, object parameters, out DynamicParameters queryParams)
+		{
+			if (parameters == null)
+			{
+				queryParams = null;
+				return RemoveTokens(sql);
+			}
+
+			string result = sql;
+
+			List<string> terms = new List<string>();
+			var queryProps = GetProperties(parameters, result, out IEnumerable<string> builtInParams);
+
+			queryParams = new DynamicParameters();
+			foreach (var prop in queryProps)
+			{
+				var value = prop.GetValue(parameters);
+				if (value != null) queryParams.Add(prop.Name, value);
+			}
+
+			var whereBuilder = GetWhereTokens();
+
+			string token;
+			if (FindWhereToken(result, out token))
+			{
+				// loop through this query's properties, but ignore base properties (like ResolvedSql and TraceCallback) since they are never part of WHERE clause
+				foreach (var pi in queryProps)
+				{
+					object value = pi.GetValue(parameters);
+					if (HasValue(value))
+					{
+						// built-in params are not part of the WHERE clause, so they are excluded from added terms
+						if (!builtInParams.Contains(pi.Name.ToLower()))
+						{
+							var cases = pi.GetCustomAttributes(typeof(CaseAttribute), false).OfType<CaseAttribute>();
+							var selectedCase = cases?.FirstOrDefault(c => c.Value.Equals(value));
+							if (selectedCase != null)
+							{
+								terms.Add(selectedCase.Expression);
+							}
+							else
+							{
+								WhereAttribute whereAttr = pi.GetAttribute<WhereAttribute>();
+								if (whereAttr != null) terms.Add(whereAttr.Expression);
+							}
+						}
+					}
+				}
+
+				result = ResolveWhereToken(token, result, terms);
+			}
+
+			return result;
+		}
+
+		public static bool FindWhereToken(string sql, out string token)
+		{
+			var tokens = GetWhereTokens();
+			return sql.ContainsAny(tokens.Select(kp => kp.Key), out token);
+		}
+
+		public static string ResolveWhereToken(string token, string sql, IEnumerable<string> criteria)
+		{
+			var tokens = GetWhereTokens();
+			return sql.Replace(token, (criteria.Any()) ? $"{tokens[token]} {string.Join(" AND ", criteria)}" : string.Empty);
+		}
+
+		private static Dictionary<string, string> GetWhereTokens()
+		{
+			return new Dictionary<string, string>()
+			{
+				{ InternalStringExtensions.WhereToken, "WHERE" }, // query has no WHERE clause, so it will be added
+				{ InternalStringExtensions.AndWhereToken, "AND" } // query already contains a WHERE clause, we're just adding to it
+			};
+		}
+
+		private static string RemoveTokens(string sql)
+		{
+			string result = sql;
+
+			var tokens = GetWhereTokens();
+			foreach (var kp in tokens) result = result.Replace(kp.Key, string.Empty);
+
+			return result;
+		}
+
+		private static bool HasValue(object value)
+		{
+			if (value != null)
+			{
+				if (value.Equals(string.Empty)) return false;
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Returns the properties of a query object based on parameters defined in a
+		/// SQL statement as well as properties with Where and Case attributes
+		/// </summary>
+		private static IEnumerable<PropertyInfo> GetProperties(object query, string sql, out IEnumerable<string> builtInParams)
+		{
+			// this gets the param names within the query based on words with leading '@'
+			builtInParams = sql.GetParameterNames(true).Select(p => p.ToLower());
+			var builtInParamsArray = builtInParams.ToArray();
+
+			// these are the properties of the Query that are explicitly defined and may impact the WHERE clause
+			var queryProps = query.GetType().GetProperties().Where(pi =>
+				pi.HasAttribute<WhereAttribute>() ||
+				pi.HasAttribute<CaseAttribute>() ||
+				builtInParamsArray.Contains(pi.Name.ToLower()));
+
+			return queryProps;
+		}
+	}
+}
