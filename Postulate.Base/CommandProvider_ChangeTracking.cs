00a824b4600c77078d3483b3091f8220eccf121b
Adam O'Neil <adamosoftware@gmail.com>
2019-04-02 14:11:21 -0400

    Base version 1.3.15 fixed issue with how ITrackedRecord.TrackChangesAsync is called

--- Postulate.Base/CommandProvider_ChangeTracking.cs
+++ Postulate.Base/CommandProvider_ChangeTracking.cs
@@ -82,7 +82,10 @@ namespace Postulate.Base
 
 			trackedRecord?.TrackChanges(connection, version, changes, user);
 
-			await trackedRecord?.TrackChangesAsync(connection, version, changes, user);
+			if (trackedRecord != null)
+			{
+				await trackedRecord.TrackChangesAsync(connection, version, changes, user);
+			}			
 		}
 
 		private void SaveChanges<TModel>(IDbConnection connection, TModel @object, IEnumerable<PropertyChange> changes, IUser user)
